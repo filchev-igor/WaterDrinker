5f3c6d5421db14793a3728a442c55ad26add3a03
Filchev Igor <igor.filchev@zenitech.co.uk>
2025-01-27 21:55:37 +0200

    Notifications

---
+++ app/src/main/java/com/example/waterdrinker/NotificationsActivity.kt
@@ -0,0 +1,26 @@
+package com.example.waterdrinker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class NotificationsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_notifications)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val fab: FloatingActionButton = findViewById(R.id.floatingActionButton)
+        fab.setOnClickListener {
+
+        }
+    }
+}
